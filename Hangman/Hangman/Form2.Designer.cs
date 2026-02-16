5707d691959fdda976c651ab1a7001f759b8b1bb
eduardo ferreira <belenuslugh@gmail.com>
2026-02-16 18:17:06 -0300

    Adiciona nova imagens da forca  e botão  da  na versão C# do jogo da forca

--- Hangman/Hangman/Form2.Designer.cs
+++ Hangman/Hangman/Form2.Designer.cs
@@ -31,6 +31,8 @@
             this.label2 = new System.Windows.Forms.Label();
             this.btn = new System.Windows.Forms.Button();
             this.btnVoltar = new System.Windows.Forms.Button();
+            this.picForca = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picForca)).BeginInit();
             this.SuspendLayout();
             // 
             // label2
@@ -39,7 +41,7 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("MingLiU_MSCS-ExtB", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(200)))), ((int)(((byte)(186)))));
-            this.label2.Location = new System.Drawing.Point(196, 37);
+            this.label2.Location = new System.Drawing.Point(176, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(402, 27);
             this.label2.TabIndex = 1;
@@ -47,7 +49,7 @@
             // 
             // btn
             // 
-            this.btn.Location = new System.Drawing.Point(561, 364);
+            this.btn.Location = new System.Drawing.Point(616, 364);
             this.btn.Name = "btn";
             this.btn.Size = new System.Drawing.Size(122, 58);
             this.btn.TabIndex = 2;
@@ -56,25 +58,36 @@
             // 
             // btnVoltar
             // 
-            this.btnVoltar.Location = new System.Drawing.Point(406, 364);
+            this.btnVoltar.Location = new System.Drawing.Point(616, 287);
             this.btnVoltar.Name = "btnVoltar";
             this.btnVoltar.Size = new System.Drawing.Size(122, 58);
             this.btnVoltar.TabIndex = 3;
             this.btnVoltar.Text = " Voltar";
             this.btnVoltar.UseVisualStyleBackColor = true;
-            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
+            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click_1);
+            // 
+            // picForca
+            // 
+            this.picForca.Location = new System.Drawing.Point(25, 56);
+            this.picForca.Name = "picForca";
+            this.picForca.Size = new System.Drawing.Size(223, 192);
+            this.picForca.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picForca.TabIndex = 4;
+            this.picForca.TabStop = false;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(80)))), ((int)(((byte)(122)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 521);
+            this.Controls.Add(this.picForca);
             this.Controls.Add(this.btnVoltar);
             this.Controls.Add(this.btn);
             this.Controls.Add(this.label2);
             this.Name = "Form2";
             this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.picForca)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -85,5 +98,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btn;
         private System.Windows.Forms.Button  btnVoltar;
+        private System.Windows.Forms.PictureBox picForca;
     }
 }
--- Hangman/Hangman/Form2.cs
+++ Hangman/Hangman/Form2.cs
@@ -15,13 +15,133 @@ namespace Hangman
         public Form2()
         {
             InitializeComponent();
+            AtualizarForca();
+            InicializarJogo();
+            CriarBotoesLetras();
+            CriarEspacosLetras();
         }
 
-        private void btnVoltar_Click(object sender, EventArgs e)
+
+        private int erros = 0;
+        private string palavraSecreta = "COMPUTADOR"; // Palavra a adivinhar
+        private string palavraAtual = ""; // Letras descobertas
+        private Button[] botoesLetras = new Button[26]; // Array dos 26 botões
+
+
+
+        private void InicializarJogo()
+        {
+            // Cria a palavra com underscores
+            palavraAtual = new string('_', palavraSecreta.Length);
+
+            // Ativa captura de teclas
+            this.KeyPreview = true;
+            //this.KeyPress += Form2_KeyPress;
+        }
+
+        private void CriarEspacosLetras()
+        {
+            int x = 205; // Posição inicial X
+            int y = 200; // Posição inicial Y
+
+            for (int i = 0; i < palavraSecreta.Length; i++)
+            {
+                Label lbl = new Label();
+                lbl.Text = "_";
+                lbl.Font = new Font("Arial", 24, FontStyle.Bold);
+                lbl.Size = new Size(40, 40);
+                lbl.Location = new Point(x, y);
+                lbl.Name = "lblLetra" + i;
+                lbl.TextAlign = ContentAlignment.MiddleCenter;
+                lbl.BorderStyle = BorderStyle.FixedSingle;
+
+                this.Controls.Add(lbl);
+
+                x += 50; // Espaçamento entre letras
+            }
+        }
+
+        private void CriarBotoesLetras()
+        {
+            string alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int x = 50;  // Posição X inicial
+            int y = 280; // Posição Y inicial
+            int largura = 40;
+            int altura = 40;
+            int espacamento = 5;
+            int botoesPorLinha = 13; // 13 botões por linha
+
+            for (int i = 0; i < 26; i++)
+            {
+                Button btn = new Button();
+                btn.Text = alfabeto[i].ToString();
+                btn.Name = "btn" + alfabeto[i];
+                btn.Size = new Size(largura, altura);
+                btn.Font = new Font("Arial", 12, FontStyle.Bold);
+                btn.BackColor = Color.LightBlue;
+
+                // Posição do botão
+                int coluna = i % botoesPorLinha;
+                int linha = i / botoesPorLinha;
+                btn.Location = new Point(x + (coluna * (largura + espacamento)),
+                                         y + (linha * (altura + espacamento)));
+
+
+                // Adiciona ao formulário
+                this.Controls.Add(btn);
+                botoesLetras[i] = btn;
+            }
+        }
+
+
+        private void AtualizarForca()
+        {
+            switch (erros)
+            {
+                case 0:
+                    picForca.Image = Properties.Resources.FORCA_0;
+                    break;
+                case 1:
+                    picForca.Image = Properties.Resources.FORCA_1;
+                    break;
+                case 2:
+                    picForca.Image = Properties.Resources.FORCA_2;
+                    break;
+                case 3:
+                    picForca.Image = Properties.Resources.FORCA_3;
+                    break;
+                case 4:
+                    picForca.Image = Properties.Resources.FORCA_4;
+                    break;
+                case 5:
+                    picForca.Image = Properties.Resources.FORCA_5;
+                    break;
+                case 6:
+                    picForca.Image = Properties.Resources.FORCA_6;
+                    MessageBox.Show("Você perdeu!");
+                    break;
+            }
+        }
+
+
+
+        // Chame este método quando o jogador errar uma letra
+        private void AdicionarErro()
+        {
+            erros++;
+            AtualizarForca();
+        }
+        private void btnA_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btnVoltar_Click_1(object sender, EventArgs e)
         {
             Form1 telaJogo = new Form1();
-            telaJogo.Show(); 
-            this.Hide(); 
+            telaJogo.Show();
+            this.Hide();
         }
     }
 }
